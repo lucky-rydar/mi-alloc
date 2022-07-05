9a562506f3ff1e4bd0105c37a54ee16d85b4581a
Mykhailo Didur <mikhaildidur2003@gmail.com>
2022-07-05 10:23:31 +0300

    Add performance tests
    Add performance test that compares malloc with custom allocator.
    
    Increase custom allocator memory to 100Mb

--- test/allocTest.cpp
+++ test/allocTest.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <cstdlib>
+#include <functional>
+#include <vector>
+#include <list>
+#include <chrono>
+
 #include "gtest/gtest.h"
 
 #include "alloc.h"
@@ -122,4 +128,151 @@ TEST(Alloc, random_test)
         if(mem_info.free_mem > DEFAULT_SIZE)
             ASSERT_TRUE(false);
     }
+}
+
+struct performance_data
+{
+    size_t num_tests;
+    size_t num_allocs;
+    size_t num_deallocs;
+    float avg_time_alloc;
+    float avg_time_del;
+};
+
+std::vector<std::pair<size_t, size_t>> sizes = {
+    {1, 8},
+    {32768, 65536},
+    {1048576, 2097152}
+};
+
+performance_data allocation_test(size_t test_amount, std::function<void*(size_t)> alloc_func, std::function<void(void*)> del_func, size_t size_i) {
+    clear();
+
+    std::vector<int> dealloc_measures;
+    std::vector<int> alloc_measures;
+    std::list<void*> pointers;
+    for(size_t i = 0; i < test_amount; i++) {
+        bool do_alloc = (rand() % 2) == 0;
+        if(!do_alloc && pointers.size() > 0) {
+            // get iterator to random element
+            auto it = pointers.begin();
+            std::advance(it, rand() % pointers.size());
+
+            // delete it
+            auto begin = std::chrono::steady_clock::now();
+            del_func(*it);
+            auto end = std::chrono::steady_clock::now();
+            pointers.erase(it);
+            auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+            dealloc_measures.push_back(duration.count());
+        } else if (do_alloc) { // do alloc
+            auto begin = std::chrono::steady_clock::now();
+            auto min_size = sizes[size_i].first;
+            auto max_size = sizes[size_i].second;
+            int var_size = rand() % (max_size - min_size) + min_size;
+            auto p = alloc_func(var_size);
+            auto end = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+            alloc_measures.push_back(duration.count());
+            pointers.push_back(p);
+        }
+    }
+
+    // print average results
+    int sum_alloc = 0;
+    int sum_dealloc = 0;
+    for(auto& measure : alloc_measures) {
+        sum_alloc += measure;
+    }
+    for(auto& measure : dealloc_measures) {
+        sum_dealloc += measure;
+    }
+
+    performance_data data;
+    data.num_tests = test_amount;
+    data.num_allocs = alloc_measures.size();
+    data.num_deallocs = dealloc_measures.size();
+    data.avg_time_alloc = (float)sum_alloc / alloc_measures.size();
+    data.avg_time_del = (float)sum_dealloc / dealloc_measures.size();
+
+    return data;
+}
+
+TEST(Alloc, performance_test)
+{
+    size_t subtests_amount = 1000;
+    size_t tests_amount = 10;
+    size_t big_variables = 1;
+
+    float avg_custom_alloc = 0;
+    float avg_custom_del = 0;
+    float avg_std_alloc = 0;
+    float avg_std_del = 0;
+
+    printf("Alloc performance test\n");
+    {
+        printf("\nCustom allocator:\n");
+        std::vector<float> allocation_measures;
+        std::vector<float> deallocation_measures;
+        for(size_t i = 0; i < tests_amount; i++) {
+            auto data = allocation_test(subtests_amount, alloc, del, big_variables);
+            allocation_measures.push_back(data.avg_time_alloc);
+            deallocation_measures.push_back(data.avg_time_del);
+        }
+
+        float sum_alloc = 0;
+        float sum_dealloc = 0;
+        for(auto& measure : allocation_measures) {
+            sum_alloc += measure;
+        }
+        for(auto& measure : deallocation_measures) {
+            sum_dealloc += measure;
+        }
+
+        avg_custom_alloc = (float)sum_alloc / allocation_measures.size();
+        avg_custom_del = (float)sum_dealloc / deallocation_measures.size();
+
+        // print average results
+        printf("allocation time: %f\n", avg_custom_alloc);
+        printf("deallocation time: %f\n", avg_custom_del);
+    }
+
+    {
+        printf("\nC native allocator\n");
+        std::vector<float> allocation_measures;
+        std::vector<float> deallocation_measures;
+        for(size_t i = 0; i < tests_amount; i++) {
+            auto data = allocation_test(subtests_amount, malloc, free, big_variables);
+            allocation_measures.push_back(data.avg_time_alloc);
+            deallocation_measures.push_back(data.avg_time_del);
+        }
+
+        float sum_alloc = 0;
+        float sum_dealloc = 0;
+        for(auto& measure : allocation_measures) {
+            sum_alloc += measure;
+        }
+        for(auto& measure : deallocation_measures) {
+            sum_dealloc += measure;
+        }
+
+        avg_std_alloc = (float)sum_alloc / allocation_measures.size();
+        avg_std_del = (float)sum_dealloc / deallocation_measures.size();
+
+        // print average results
+        printf("allocation time: %f\n", avg_std_alloc);
+        printf("deallocation time: %f\n", avg_std_del);
+    }
+
+    printf("\n");
+
+    // print in % how much the custom allocator is faster
+    printf("Custom allocator is %f%% faster\n", (avg_std_alloc - avg_custom_alloc) / avg_std_alloc * 100);
+
+    // print in % how much the custom deallocator is faster
+    printf("Custom deallocator is %f%% faster\n", (avg_std_del - avg_custom_del) / avg_std_del * 100);
+
+    // print in % how the sum of allocation and deallocation is faster
+    printf("Sum of allocation and deallocation is %f%% faster\n",
+        (avg_std_alloc + avg_std_del - avg_custom_alloc - avg_custom_del) / (avg_std_alloc + avg_std_del) * 100);
 }
